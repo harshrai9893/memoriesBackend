4f071b7e7fca792fb7f6a19549e2ca04f20f43ee
harshrai9893 <harshofficial0509@gmail.com>
2026-01-13 20:55:36 +0530

    mongoDB connect

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from "dotenv";
 
 import postRoutes from './routes/posts.js';
+
+dotenv.config();
 
 const app = express();
 
@@ -14,7 +17,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
+const CONNECTION_URL = process.env.MONGO_URI;
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL)
